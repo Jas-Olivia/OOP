c73a0cd584a4438a6db5f8b5f8d71208b5679187
Jas-Olivia <jas@Pro7-Jas>
2025-03-04 15:27:44 +1030

    workshop1 files

--- main.cpp
+++ main.cpp
@@ -9,14 +9,14 @@ int main() {
     int array1[5] = {4,5,6,7,8};
     std::cout << "The number is: " << count(array1, 5) << std::endl;
    // test case 1 
-    int array2[5] = {-1,3,-4,10,12};
-    std::cout << "The number is: " << count(array2, 5) << std::endl;
+   // int array2[5] = {-1,3,-4,10,12};
+   // std::cout << "The number is: " << count(array2, 5) << std::endl;
    // test case 2 
-    int array3[10] = {4,5,6,7,8,-1,-2,0,0,0};
-    std::cout << "The number is: " << count(array3, 10) << std::endl;
+   // int array3[10] = {4,5,6,7,8,-1,-2,0,0,0};
+   // std::cout << "The number is: " << count(array3, 10) << std::endl;
    // test case 3
-   int array4[5] = {1,1,1,1,1};
-    std::cout << "The number is: " << count(array4, 5) << std::endl;
+   // int array4[5] = {1,1,1,1,1};
+   // std::cout << "The number is: " << count(array4, 5) << std::endl;
    
     return 0;
 }
