0034b59e4b87afd8dd3a73efbcbd37072dbfe969
Jas-Olivia <jas@Pro7-Jas>
2025-03-05 17:56:35 +1030

    Tricky questions

--- main-3-5.cpp
+++ main-3-5.cpp
@@ -4,7 +4,7 @@ using namespace std;
 double sum_even(double array[], int length);
 
 int main() {
-    double array[6] = {2,2,1,4,1,3};
+    double array[6] = {1,3,5,7,9,11};
     cout << sum_even(array,6);
     return 0;
 }
