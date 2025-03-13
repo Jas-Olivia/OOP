7d9aa8a8f03c3ab1e962403ccc33561e3e215876
Jas-Olivia <jas@Pro7-Jas>
2025-03-13 15:14:42 +1030

    easy questions

---
+++ main-1-4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+extern void print_scaled(int array[3][3],int scale);
+
+int main(){
+    int array [3][3] = {
+    {0,2,3},
+    {4,5,6},
+    {7,8,9},
+    };
+    print_scaled(array,3);
+    return 0;
+}
